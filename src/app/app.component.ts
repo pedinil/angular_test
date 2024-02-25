7e7dc3ae3b968ef67527be031d9f8492eeec52cd
Pedram Ezzati <pedram.ezzati@ceitasl.com>
2024-02-25 22:22:38 +0330

    adding filter on combobox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,36 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  items : any= [ 
-          // new ModelData('learn',true),
-          // new ModelData('cafe',true),
-          // new ModelData('tea',true)
+  items : ModelData[]= [ 
+          new ModelData('learn',true),
+          new ModelData('cafe',true),
+          new ModelData('tea',true)
         ];
 
   title = 'mywhishlist';
 
+  selectFilter :string = '0';
+
   newItem = '';
+
+  visibleItems : ModelData[]= this.items;
   
   addNewItem(){
     this.items.push(new ModelData(this.newItem,false));
     this.newItem='';
+  }
+
+  filterChange(value : any)
+  {
+    if(value==0)
+    {
+      this.visibleItems=this.items;
+    }else if(value==1)
+    {
+      this.visibleItems=this.items.filter(item => !item.isComplete);
+    }else if(value==2)
+    {this.visibleItems=this.items.filter(item => item.isComplete);}
+     
   }
 
   toggleItem(item : ModelData){
