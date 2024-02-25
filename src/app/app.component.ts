cb521d0c9da1bbc50a46a134c7b7be9e36a8bd83
Pedram Ezzati <pedram.ezzati@ceitasl.com>
2024-02-25 21:35:38 +0330

    make checkbox dynamic by adding from list by ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,34 @@ import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ModelData } from '../models/modeldata';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items : any= [ 
-          new ModelData('learn',true),
-          new ModelData('cafe',true),
-          new ModelData('tea',true)
+          // new ModelData('learn',true),
+          // new ModelData('cafe',true),
+          // new ModelData('tea',true)
         ];
 
   title = 'mywhishlist';
+
+  newItem = '';
+  
+  addNewItem(){
+    this.items.push(new ModelData(this.newItem,false));
+    this.newItem='';
+  }
 
   toggleItem(item : ModelData){
     item.isComplete=!item.isComplete;
     console.log(item)
   }
+  
 }
